dc7827bd776cadf372ed5f781b2168656741cf1b
Your Name <you@example.com>
2021-08-15 15:43:24 +0800

    First commit

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import "./style.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <Typography className="title" variant="h1">
+              Welcome to DwinaTech Shop
+            </Typography>
+            <Button className="shopping-button" href="#products">
+              Shopping
+            </Button>
+          </Grid>
+          <Grid className="brand" item sm={6}>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
